802bd5c97a4b507785d31c1721bbac7b2148aa07
Денис Долгополов <dolgopolov@ssmp.local>
2021-08-23 14:05:12 +0300

    create CRUD API

--- stores/views.py
+++ stores/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .serializers import PizzeriaListSerializer, PizzeriaDetailSerializer
+from .models import Pizzeria
+
+
+class PizzeriaMixin:
+    queryset = Pizzeria.objects.all()
+    serializer_class = PizzeriaDetailSerializer
+
+
+class PizzeriaListAPIView(PizzeriaMixin, generics.ListAPIView):
+    serializer_class = PizzeriaListSerializer
+
+
+class PizzeriaRetrieveAPIView(PizzeriaMixin, generics.RetrieveAPIView):
+    pass
+
+
+class PizzeriaCreateAPIView(PizzeriaMixin, generics.CreateAPIView):
+    pass
+
+
+class PizzeriaRetrieveUpdateAPIView(PizzeriaMixin, generics.RetrieveUpdateAPIView):
+    pass
+
+
+class PizzeriaDestroyAPIView(PizzeriaMixin, generics.DestroyAPIView):
+    pass
