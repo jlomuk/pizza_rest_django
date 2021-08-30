320f87a64d0805bf18e44a4c18ab061765eb6e94
Денис Долгополов <dolgopolov@ssmp.local>
2021-08-30 09:58:56 +0300

    add drawer with screens

--- pizza_mobile/App.js
+++ pizza_mobile/App.js
@@ -3,11 +3,24 @@ import React from 'react';
 import { StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import ListView from './src/screens/components/list_view';
 import DetailView from './src/screens/components/detail_view';
+import ScreenA from "./src/screens/drawer/screenA";
+import ScreenB from "./src/screens/drawer/screenB";
+import ScreenC from "./src/screens/drawer/screenC";
+
 
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+renderScreenComponents = () => {
+    
+}
+
+
+
 
 export default function App() {
   return (
